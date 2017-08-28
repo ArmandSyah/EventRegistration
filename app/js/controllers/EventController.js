632a6224f40c71c7d473784b876ca102cab5950a
ArmandSyah <Armand.Syahtama@gmail.com>
2017-08-28 12:45:13 -0400

    Played around with directives

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,6 +3,9 @@
 eventsApp.controller('EventController',
     function EventController($scope) {
 
+        $scope.snippet = '<span style="color: red;>He yo!</span>"'
+        $scope.boolean = true;
+        $scope.mystyle = {color:'red'};
         $scope.event = {
             name: 'JS Boot Camp',
             date: '1/1/1970',
@@ -11,7 +14,38 @@ eventsApp.controller('EventController',
                 address: 'My basement',
                 city: 'My City',
                 province: 'ON'
-            }
+            },
+            sessions: [
+                {
+                    name: 'JS Event',
+                    duration: '1h 40min',
+                    prof: 'Burak Obama',
+                    difficulty: 'Advanced',
+                    upvoteCount: 0
+                },
+                {
+                    name: 'Java Event',
+                    duration: '10min',
+                    prof: 'John Doe',
+                    difficulty: 'Insane',
+                    upvoteCount: 0
+                },
+                {
+                    name: 'Donald Glover Appreciation',
+                    duration: '24hr',
+                    prof: 'Donald Glover',
+                    difficulty: 'Introductory',
+                    upvoteCount: 0
+                }
+            ]
+        }
+
+        $scope.upvoteSession = function (session) {
+            session.upvoteCount++;
+        }
+
+        $scope.downvoteSession = function (session) {
+            session.upvoteCount--;
         }
 
     });
