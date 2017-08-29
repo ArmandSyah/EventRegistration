5a380fa9ec1edbe4463ba237923e688f7b888ca1
ArmandSyah <Armand.Syahtama@gmail.com>
2017-08-29 16:35:15 -0400

    Finished Controllers

---
+++ app/js/controllers/EditEventController.js
@@ -0,0 +1,10 @@
+eventsApp.controller('EditEventController',
+    function EditEventController ($scope) {
+        $scope.saveEvent = function (event, newEventForm) {
+            if(newEventForm.$Valid())
+        }
+
+        $scope.cancelEdit = function () {
+            window.location = "/EventDetails.html";
+        }
+})
